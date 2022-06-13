70f5281fcc933736c259e5547b6e76bcbd0ced21
Rian214 <59103887+Rian214@users.noreply.github.com>
2022-06-13 08:38:59 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+import firebase_admin
+import pyrebase
+import json
+from flask import Flask, request, jsonify
+
+from firebase_admin import credentials, auth
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+#KONEKSI KE FIREBASE
+cred = credentials.Certificate('fbAdminConfig.json')
+firebase = firebase_admin.initialize_app(cred)
+pb = pyrebase.initialize_app(json.load(open('fbConfig.json')))
+
+
+
+
+#REGISTRASI AKUN
+@app.route('/register',methods=['POST'])
+def register():
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+
+    if email is None or password is None:
+        return {'msg':'Email atau Password Belum Diisi'},400
+    try:
+        user = auth.create_user(email=email,password=password)
+        return {'msg':'Akun Berhasil Dibuat!'},200
+    except:
+        return {'msg':'Email Sudah Ada'},400
+
+#LOGIN AKUN
+@app.route("/login" ,methods=["POST"])
+def login():
+    email = request.get_json()["email"]
+    password = request.get_json()["password"]
+
+    try:
+        user = pb.auth().sign_in_with_email_and_password(email,password)
+        return {"msg":"Anda Berhasil Login!"}, 200
+    except:
+        return {'msg':'Email atau Password Salah'}, 400
+
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port=8080)
